6ce92486098c86f618f70d7a7c08eac268599155
stvsever <148014764+stvsever@users.noreply.github.com>
2024-05-03 03:28:28 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,13 +1,18 @@
 # ALGORITME _ GEDRAGSECONOMIE
 
-# Dit python script is implementeerbaar, na optimalisering weliswaar, als een criterium van automatische reviews van opgestelde surveys. (DISCLAIMER: dit is een proof-of-concept ; we doen geen claims dat het al implementeerbaar zou zijn)
+# Dit python script is implementeerbaar, na optimalisering weliswaar, als een criterium van automatische reviews van opgestelde surveys. 
+# (DISCLAIMER: dit is een proof-of-concept ; we doen geen claims dat het al implementeerbaar zou zijn)
 # Wat doet het? Het gaat op zoek naar een question-order bias. De meest methodologisch verantwoorde survey flow van een set aan vragen is dat de globale vraag eerst komt.
 # Als de globale vraag niet eerst komt, kan er mogelijk een vertekening optreden doordat er reeds concrete informatie actief is die de subsequente oproeping van informatie beïnvloedt.
 # Dit fenomeen valt onder de beschikbaarheidsheuristiek. In dit script wordt opzoek gegaan naar enerzijds het bestaan van een globale vraag en anderzijds de locatie van de globale vraag.
 # Als de globale vraag bestaat EN zijn indexpositie niet nul is dan kan er een mogelijke vertekening ontstaan door specifieke vragen.
 # Dit script heeft twee toepassingen:
-# 1. (toepassing in prospect:) Op bv Qualtrics of Survio (i.e. websites waar men vragenlijsten kan opstellen) kan een USERWARNING gegenereerd worden in de automatische expert review sectie 'methodology'. Deze warning vermeldt dat er mogelijks een question-order bias kan optreden.
-# 2. (toepassing in retrospect:) Via grote databanken waarin sets van vragenlijsten worden opgeslagen kan een screening gedaan worden van bestaande vragenlijsten. De vragenlijsten waarin zo'n specific-general sequentie zit kan als logistiek beginpunt gebruikt worden voor onderzoekers die een studie uitvoeren omtrent de specific->general bias.
+# 1. (toepassing in prospect:) 
+#     Op bv Qualtrics of Survio (i.e. websites waar men vragenlijsten kan opstellen) kan een USERWARNING gegenereerd worden in de automatische expert review sectie 'methodology'. 
+#     Deze warning vermeldt dan dat er mogelijks een question-order bias kan optreden bij vraag {index}.
+# 2. (toepassing in retrospect:) 
+#     Via grote databanken waarin sets van vragenlijsten worden opgeslagen kan een screening gedaan worden van bestaande vragenlijsten. 
+#     De vragenlijsten waarin zo'n specific-general sequentie zit kan als logistiek beginpunt gebruikt worden voor onderzoekers die een studie uitvoeren omtrent de 'specific->general' bias.
 
 from openai import OpenAI
 import re
